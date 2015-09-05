88fa08a609c3e0ef7d6b286ade5f2b53798c4c57
Paul Loyd <pavelko95@gmail.com>
2015-09-05 13:21:35 +0300

    feat: initial simulation

---
+++ src/simulation.js
@@ -0,0 +1,76 @@
+import * as utils from './utils';
+import BBox from './bbox';
+import Camera from './camera';
+
+import bboxTmpl from './glsl/bbox.vert';
+import colorTmpl from './glsl/color.frag';
+
+
+export default class Simulation {
+  constructor(gl) {
+    this.gl = gl;
+
+    this.camera = new Camera;
+    this.bbox = new BBox(.5, .5, .5);
+
+    this.initExtensions();
+    this.initShaders();
+    this.initBuffers();
+  }
+
+  initExtensions() {
+    let floatExt = this.gl.getExtension('OES_texture_float');
+    if (floatExt)
+      this.textureFloatType = this.gl.FLOAT;
+    else {
+      floatExt = this.gl.getExtension('OES_texture_half_float');
+      if (floatExt)
+        this.textureFloatType = floatExt.HALF_FLOAT_OES;
+      else
+        throw new Error("OES_texture_float and OES_texture_half_float is not available");
+    }
+  }
+
+  initShaders() {
+    let vs = (tmpl, consts) => utils.compileVertexShader(this.gl, tmpl(consts));
+    let fs = (tmpl, consts) => utils.compileFragmentShader(this.gl, tmpl(consts));
+    let link = (vs, fs) => utils.createProgramInfo(this.gl, vs, fs);
+
+    let bbox = vs(bboxTmpl),
+        color = fs(colorTmpl);
+
+    this.programs = {
+      bbox: link(bbox, color)
+    };
+  }
+
+  initBuffers() {
+    this.buffers = {
+      bbox: utils.createBufferInfo(this.gl, {
+        aVertexPosition: {dims: 3, data: this.bbox.corners}
+      })
+    };
+  }
+
+  resize() {
+    this.gl.viewport(0, 0, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+    this.camera.setAspect(this.gl.drawingBufferWidth/this.gl.drawingBufferHeight);
+  }
+
+  step(timestamp) {
+    this.camera.update();
+
+    this.drawBBox();
+
+    this.gl.finish();
+  }
+
+  drawBBox() {
+    let [program, buffer] = [this.programs.bbox, this.buffers.bbox];
+
+    this.gl.useProgram(program.program);
+    utils.setUniforms(program, {uVPMatrix: this.camera.matrix});
+    utils.setBuffersAndAttributes(this.gl, program, buffer);
+    utils.drawBufferInfo(this.gl, this.gl.LINES, buffer, 24);
+  }
+}
