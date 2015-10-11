6899e381f15c1b4bf9caeeef5ec97b77d50884f3
Paul Loyd <pavelko95@gmail.com>
2015-10-11 16:23:31 +0300

    wip: add `Math.log2` polyfill

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,3 +1,4 @@
+//#TODO: What about core-js (babel/polyfill)?
 import 'setimmediate';
 import now from 'performance-now';
 import raf from 'raf';
@@ -11,3 +12,6 @@ if (!performance.now)
 
 if (!requestAnimationFrame)
   window.requestAnimationFrame = raf;
+
+if (!Math.log2)
+  Math.log2 = x => Math.log(x) / Math.LN2;
