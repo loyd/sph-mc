48fa76301e9c49a94034bf7bde6fc879e7975153
Paul Loyd <pavelko95@gmail.com>
2015-11-25 19:31:27 +0300

    feat: add i18n for russian locale

--- src/gui.js
+++ src/gui.js
@@ -1,42 +1,48 @@
 import dat from 'dat-gui';
 
+import L from './locale';
+
 
 export default class GUI {
   constructor(simulation) {
     let gui = this.gui = new dat.GUI();
 
-    let env = this.env = gui.addFolder('Environment');
-    env.add(simulation, 'gravity', -15, 0, .01);
-    env.add(simulation, 'deltaT', .002, .02, .0001);
+    let env = this.env = gui.addFolder(L`Environment`);
+    env.add(simulation, 'gravity', -15, 0, .01).name(L`Gravity`);
+    env.add(simulation, 'deltaT', .002, .02, .0001).name(L`Time step`);
     env.open();
 
-    let physics = this.physics = gui.addFolder('Fluid physics');
-    physics.add(simulation, 'temperature', 0, 80, 5);
-    physics.add(simulation, 'density0', 100, 5000, .01);
-    physics.add(simulation, 'viscosity', 1, 50, .1);
-    physics.add(simulation, 'pressureK', .5, 10, .5);
-    physics.add(simulation, 'tension', 0, .2, .01);
-    physics.add(simulation, 'restitution', 0, 1, .01);
+    let physics = this.physics = gui.addFolder(L`Fluid physics`);
+    physics.add(simulation, 'temperature', 0, 80, 5).name(L`Temperature`);
+    physics.add(simulation, 'density0', 100, 5000, .01).name(L`Density`);
+    physics.add(simulation, 'viscosity', 1, 50, .1).name(L`Viscosity`);
+    physics.add(simulation, 'pressureK', .5, 10, .5).name(L`Gas stiffness`);
+    physics.add(simulation, 'tension', 0, .2, .01).name(L`Surface tension`);
+    physics.add(simulation, 'restitution', 0, 1, .01).name(L`Restitution`);
     physics.open();
 
-    let sph = this.sph = gui.addFolder('SPH');
-    sph.add(simulation, 'nParticles', 5000, 250000, 5000);
-    sph.add(simulation, 'mass', .0005, .01, .0005);
-    sph.add(simulation, 'ratio', .02, .1, .005);
-    sph.add(simulation, 'mode', ['wireframe', 'mockup', 'dual']);
+    let sph = this.sph = gui.addFolder(L`SPH`);
+    sph.add(simulation, 'nParticles', 5000, 250000, 5000).name(L`Particle count`);
+    sph.add(simulation, 'mass', .0005, .01, .0005).name(L`Mass of particle`);
+    sph.add(simulation, 'ratio', .02, .1, .005).name(L`Support radius`);
+    sph.add(simulation, 'mode', {
+      [L`wireframe`]: 'wireframe',
+      [L`mockup`]: 'mockup',
+      [L`dual`]: 'dual'
+    }).name(L`Mode`);
     sph.open();
 
-    let mc = this.mc = gui.addFolder('MC');
-    mc.add(simulation, 'spread', 0, 5, 1);
-    mc.add(simulation, 'nVoxels', 0, 100, 1);
-    mc.add(simulation, 'range', .2, .99, .01);
+    let mc = this.mc = gui.addFolder(L`MC`);
+    mc.add(simulation, 'spread', 0, 5, 1).name(L`Spread`);
+    mc.add(simulation, 'nVoxels', 0, 100, 1).name(L`Voxel count`);
+    mc.add(simulation, 'range', .2, .99, .01).name(L`Range`);
     mc.open();
 
-    let material = this.material = gui.addFolder('Fluid material');
-    material.add(simulation, 'ambient', 0, 1, .01);
-    material.add(simulation, 'diffuse', 0, 1, .01);
-    material.add(simulation, 'specular', 0, 1, .01);
-    material.add(simulation, 'lustreless', 0, 80, 1);
+    let material = this.material = gui.addFolder(L`Optics`);
+    material.add(simulation, 'ambient', 0, 1, .01).name(L`Ambient`);
+    material.add(simulation, 'diffuse', 0, 1, .01).name(L`Diffuse`);
+    material.add(simulation, 'specular', 0, 1, .01).name(L`Specular`);
+    material.add(simulation, 'lustreless', 0, 80, 1).name(L`Lustreless`);
 
     let proxy = {color: simulation.color.map(c => c * 255)};
     let h = dat.color.color.math.component_from_hex;
@@ -46,9 +52,9 @@ export default class GUI {
         simulation.color = [2, 1, 1].map(c => h(num, c) / 255);
       } else
         simulation.color = color.map(c => c / 255);
-    });
+    }).name(L`Color`);
 
-    material.add(simulation, 'opacity', 0, 1, .01);
+    material.add(simulation, 'opacity', 0, 1, .01).name(L`Opacity`);
     material.open();
   }
 }
---
+++ src/locale.js
@@ -0,0 +1,43 @@
+import assert from 'assert';
+
+
+const useRuLang = (window.navigator.language || window.navigator.userLanguage) === 'ru';
+
+const map = {
+  'Environment': 'Среда',
+  'Gravity': 'Гравитация',
+  'Time step': 'Временной шаг',
+  'Fluid physics': 'Хар-ки жидкости',
+  'Temperature': 'Температура',
+  'Density': 'Плотность',
+  'Viscosity': 'Вязкость',
+  'Gas stiffness': 'Жёсткость',
+  'Surface tension': 'Пов. натяжение',
+  'Restitution': 'Восстановление',
+  'SPH': 'ГСЧ',
+  'Particle count': 'Кол-во частиц',
+  'Mass of particle': 'Масса частицы',
+  'Support radius': 'Радиус ядра',
+  'Mode': 'Режим',
+  'wireframe': 'каркас',
+  'mockup': 'модель',
+  'dual': 'двойной',
+  'MC': 'ШК',
+  'Spread': 'Сглаживание поверхности',
+  'Voxel count': 'Кол-во вокселей',
+  'Range': 'Предел применения',
+  'Optics': 'Оптика',
+  'Ambient': 'Фоновое осв.',
+  'Diffuse': 'Рассеянное осв.',
+  'Specular': 'Зеркальное осв.',
+  'Lustreless': 'Матовость',
+  'Color': 'Цвет',
+  'Opacity': 'Непрозрачность'
+};
+
+export default function(string) {
+  assert.equal(string.length, 1);
+  assert(string[0] in map);
+
+  return useRuLang ? map[string[0]] : string[0];
+}
